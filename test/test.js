a1767a250d6868ee5c1d0bee4a9782ae9f40aea8
Nico Hauser <me@tyratox.ch>
2017-08-27 00:13:11 +0200

    Switched from mocha to ava

--- test/test.js
+++ test/test.js
@@ -1,47 +1,36 @@
-const assert = require("assert");
-
+import test from "ava";
 import ValueFilter from "../src/lazuli-filters.mjs";
 
-describe("ValueFilter", () => {
-	describe("#get()", () => {
-		it("should get the complete filter map", () => {
-			let valueFilter = new ValueFilter();
-			const fn = x => x;
+test("ValueFilter.get() should get the complete filter map", t => {
+	let valueFilter = new ValueFilter();
+	const fn = x => x;
 
-			valueFilter._filters = { filter: [fn] };
-			assert.deepEqual(valueFilter.get(), { filter: [fn] });
-		});
-	});
-	describe("#add()", () => {
-		it("should add a function to the filter map", () => {
-			let valueFilter = new ValueFilter();
-			const fn = x => x;
+	valueFilter._filters = { filter: [fn] };
+	t.deepEqual(valueFilter.get(), { filter: [fn] });
+});
+test("ValueFilter.get().add() should add a function to the filter map", t => {
+	let valueFilter = new ValueFilter();
+	const fn = x => x;
 
-			valueFilter.add("filter", fn);
-			assert.deepEqual(valueFilter._filters, { filter: [fn] });
-			valueFilter.add("filter", fn);
-			assert.deepEqual(valueFilter._filters, { filter: [fn, fn] });
-		});
-	});
-	describe("#remove()", () => {
-		it("should remove a function from the filter map", () => {
-			let valueFilter = new ValueFilter();
-			const fn = x => x;
-			const fn2 = y => y;
+	valueFilter.add("filter", fn);
+	t.deepEqual(valueFilter._filters, { filter: [fn] });
+	valueFilter.add("filter", fn);
+	t.deepEqual(valueFilter._filters, { filter: [fn, fn] });
+});
+test("ValueFilter.remove() should remove a function from the filter map", t => {
+	let valueFilter = new ValueFilter();
+	const fn = x => x;
+	const fn2 = y => y;
 
-			valueFilter._filters = { filter: [fn, fn, fn2] };
-			valueFilter.remove("filter", fn);
-			assert.deepEqual(valueFilter._filters, { filter: [fn2] });
-		});
-	});
-	describe("#filterable()", () => {
-		it("should filter the value with all relevant filters", () => {
-			let valueFilter = new ValueFilter();
-			const fn = x => 1;
-			const fn2 = y => y * 2;
+	valueFilter._filters = { filter: [fn, fn, fn2] };
+	valueFilter.remove("filter", fn);
+	t.deepEqual(valueFilter._filters, { filter: [fn2] });
+});
+test("ValueFilter.filterable() should filter the value with all relevant filters", t => {
+	let valueFilter = new ValueFilter();
+	const fn = x => 1;
+	const fn2 = y => y * 2;
 
-			valueFilter._filters = { filter: [fn, fn2] };
-			assert.deepEqual(valueFilter.filterable("filter", 0), 2); //0 => 1 => 2
-		});
-	});
+	valueFilter._filters = { filter: [fn, fn2] };
+	t.deepEqual(valueFilter.filterable("filter", 0), 2); //0 => 1 => 2
 });
